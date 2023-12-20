4ea1f7bfb0289e58cb078ae34809ba327b5bd370
Richard William <rwilliam.flores@gmail.com>
2023-12-20 20:06:39 +0800

    Refactor import statements and add Clerk integration

--- pages/products.js
+++ pages/products.js
@@ -1,17 +1,15 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import styled from "styled-components";
-import Center from "@/components/Center";
-import {mongooseConnect} from "@/lib/mongoose";
-import {Product} from "@/models/Product";
-import ProductsGrid from "@/components/ProductsGrid";
-import Title from "@/components/Title";
-import {getServerSession} from "next-auth";
-import {authOptions} from "@/pages/api/auth/[...nextauth]";
-import {WishedProduct} from "@/models/WishedProduct";
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Center from '@/components/Center';
+import { mongooseConnect } from '@/lib/mongoose';
+import { Product } from '@/models/Product';
+import ProductsGrid from '@/components/ProductsGrid';
+import Title from '@/components/Title';
+import { WishedProduct } from '@/models/WishedProduct';
+import { clerkClient } from '@clerk/nextjs';
+import { getAuth } from '@clerk/nextjs/server';
 
-
-export default function ProductsPage({products,wishedProducts}) {
+export default function ProductsPage({ products, wishedProducts }) {
   return (
     <>
       <Header />
@@ -46,24 +44,24 @@ export default function ProductsPage({products,wishedProducts}) {
       <br></br>
       <Footer />
     </>
-    
   );
 }
 
 export async function getServerSideProps(ctx) {
   await mongooseConnect();
-  const products = await Product.find({}, null, {sort:{'_id':-1}});
-  const session = await getServerSession(ctx.req, ctx.res, authOptions);
-  const wishedProducts = session?.user
-      ? await WishedProduct.find({
-          userEmail:session?.user.email,
-          product: products.map(p => p._id.toString()),
-        })
-      : [];
+  const products = await Product.find({}, null, { sort: { _id: -1 } });
+  const { userId } = getAuth(ctx.req);
+  const user = await clerkClient.users.getUser(userId);
+  const wishedProducts = user
+    ? await WishedProduct.find({
+        userEmail: user.primaryEmailAddressId,
+        product: products.map(p => p._id.toString()),
+      })
+    : [];
   return {
-    props:{
+    props: {
       products: JSON.parse(JSON.stringify(products)),
       wishedProducts: wishedProducts.map(i => i.product.toString()),
-    }
+    },
   };
 }
