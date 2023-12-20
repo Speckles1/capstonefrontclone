92716b430d17745b044ee8edabfbc66fd70edbd0
Richard William <rwilliam.flores@gmail.com>
2023-12-20 15:17:39 +0800

    Fixed session being cleared

--- components/CartContext.js
+++ components/CartContext.js
@@ -1,10 +1,10 @@
-import {createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from 'react';
 
 export const CartContext = createContext({});
 
-export function CartContextProvider({children}) {
-  const ls = typeof window !== "undefined" ? window.localStorage : null;
-  const [cartProducts,setCartProducts] = useState([]);
+export function CartContextProvider({ children }) {
+  const ls = typeof window !== 'undefined' ? window.localStorage : null;
+  const [cartProducts, setCartProducts] = useState([]);
   useEffect(() => {
     if (cartProducts?.length > 0) {
       ls?.setItem('cart', JSON.stringify(cartProducts));
@@ -16,23 +16,25 @@ export function CartContextProvider({children}) {
     }
   }, []);
   function addProduct(productId) {
-    setCartProducts(prev => [...prev,productId]);
+    setCartProducts(prev => [...prev, productId]);
   }
   function removeProduct(productId) {
     setCartProducts(prev => {
       const pos = prev.indexOf(productId);
       if (pos !== -1) {
-        return prev.filter((value,index) => index !== pos);
+        return prev.filter((value, index) => index !== pos);
       }
       return prev;
     });
   }
   function clearCart() {
     setCartProducts([]);
-    ls.clear();
+    ls.removeItem('cart');
   }
   return (
-    <CartContext.Provider value={{cartProducts,setCartProducts,addProduct,removeProduct,clearCart}}>
+    <CartContext.Provider
+      value={{ cartProducts, setCartProducts, addProduct, removeProduct, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
